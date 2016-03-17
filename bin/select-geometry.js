5456dc74cd0ea053f90b78da3d280b0f5d20171d
Todd Wolfson <todd@twolfson.com>
2016-03-17 10:55:45 -0500

    Added script for debugging select geometry

---
+++ bin/select-geometry.js
@@ -0,0 +1,22 @@
+// DEV: This file should be run via `npm run select-geometry`
+// Load in our dependencies
+var app = require('electron').app;
+var selectGeometry = require('../lib/select-geometry');
+
+// When all windows are closed, exit out
+app.on('window-all-closed', function handleWindowsClosed () {
+  app.quit();
+});
+
+// When Electron is done loading, launch our geometry selector
+app.on('ready', function handleReady () {
+  selectGeometry(function handleSelectGeometry (err, geometry) {
+    // If there was an error, throw it
+    if (err) {
+      throw err;
+    }
+
+    // Otherwise, log the geometry
+    console.log('Selected geometry:', geometry);
+  });
+});
