391e465373eade244c2e83e939f9720d9f5925d5
Todd Wolfson <todd@twolfson.com>
2016-03-17 10:11:33 -0500

    Discared allowUnknownOption to fix support for outfile

--- bin/record-a-cast.js
+++ bin/record-a-cast.js
@@ -5,18 +5,12 @@ var spawn = require('child_process').spawn;
 var electronPath = require('electron-prebuilt');
 
 // Find our application
-var mockdeskPath = path.join(__dirname, '..');
-var args = [mockdeskPath];
-
-// If we are on Linux, then add transparency flags
-// https://github.com/atom/electron/blob/v0.37.2/docs/api/frameless-window.md#limitations
-if (process.platform === 'linux') {
-  args = args.concat(['--enable-transparent-visuals', '--disable-gpu']);
-}
+var appPath = path.join(__dirname, '..');
+var args = [appPath];
 
 // Append all arguments after our node invocation
-// e.g. `node bin/mockdesk.js --version` -> `--version`
+// e.g. `node bin/app.js --version` -> `--version`
 args = args.concat(process.argv.slice(2));
 
 // Run electron on our application and forward all stdio
-spawn(electronPath, args, {stdio: [0, 1, 2]});
+spawn(electronPath, args, {stdio: 'inherit'});
