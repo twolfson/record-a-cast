228b334a57a77b09084beec97a826367732c5912
Todd Wolfson <todd@twolfson.com>
2016-03-17 01:08:27 -0500

    Added base for get bounding rect script

--- bin/record-a-gif.js
+++ bin/record-a-gif.js
@@ -8,6 +8,12 @@ var electronPath = require('electron-prebuilt');
 var mockdeskPath = path.join(__dirname, '..');
 var args = [mockdeskPath];
 
+// If we are on Linux, then add transparency flags
+// https://github.com/atom/electron/blob/v0.37.2/docs/api/frameless-window.md#limitations
+if (process.platform === 'linux') {
+  args = args.concat(['--enable-transparent-visuals', '--disable-gpu']);
+}
+
 // Append all arguments after our node invocation
 // e.g. `node bin/mockdesk.js --version` -> `--version`
 args = args.concat(process.argv.slice(2));
